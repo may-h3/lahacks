f309e1532e09aed89cd1f70fb4dd5d0fd78c9116
May He <meiandpiano@gmail.com>
2023-04-22 17:15:59 -0700

    webNav listeners

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 console.log(chrome);
-console.log("I;m debugging");
+console.log("I'm debugging");
 
 let userActivityLog = [];
 
@@ -9,4 +9,31 @@ chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
     var domain = url.split('/')[2];
     console.log("Current page: " + url);
     console.log("Current domain: " + domain);
-})
+
+    // webNav code - ie anything happening within the tab itself
+    chrome.webNavigation.onBeforeNavigate.addListener(function() {
+        console.log("navigation detected")
+        url = tab.url;
+        userActivityLog.push("navigating away from: " + url); // push some data into array
+    });
+
+    chrome.webNavigation.onCommitted.addListener(function() {
+        console.log("commit detected");
+        url = tab.url;
+        userActivityLog.push("successfully navigated away from: " + url);
+    });
+
+    chrome.webNavigation.onDOMContentLoaded.addListener(function() {
+        console.log("dom content loaded");
+        url = tab.url;
+        userActivityLog.push(url + "'s dom loaded");
+    }); // console always displays beforenav, oncommit, domcontent in that
+    // order is that because of the way i coded it
+
+    chrome.webNavigation.onCompleted.addListener(function(timeStamp) {
+        console.log("smth completed?"); // doc + all referred resources comp loaded + initalized
+        // url = tab.url;
+        console.log(url);
+        userActivityLog.push("smth completed at: " + url);
+    });
+});
