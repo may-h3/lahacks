c5efaf6b405f35b6e80e9b97a3bd64412b78ef41
nikki-ha <nikkiha811@gmail.com>
2023-04-22 22:32:58 -0700

    Can calculate time a user spent on a webpage *sometimes*.

--- background.js
+++ background.js
@@ -2,70 +2,124 @@ console.log(chrome);
 console.log("I'm debugging");
 
 var userActivityLog = [];
+var blacklistedURLs = ["https://www.youtube.com/"];
 var timeStamps = [];
 
-// Notes
-// ===================
-// Tabs vs. WebNavigation
-// tabs = all tabs
-// webnavigation = within tab
-//      onBeforeNavigate: when you press return key to navigate away
-//          - Fired when a navigation is about to occur.
-//          - **** USE TIMESTAMP
-//      onCommitted: when the page loads
-//          - Fired when a navigation is committed. The document 
-//            (and the resources it refers to, such as images and 
-//            subframes) might still be downloading, but at least 
-//            part of the document has been received from the server 
-//            and the browser has decided to switch to the new document.
-//      onDOMContentLoaded: when content loads
-//          - Fired when the page's DOM is fully constructed, but the 
-//            referenced resources may not finish loading.
-//      onCompleted: when everything is done
-//          - Fired when a document, including the resources it refers 
-//            to, is completely loaded and initialized.
-//          - **** USE TIMESTAMP
-// active tabs = tab currently on
+var isBlackListed = false;
+var isCommitted = false;
+var startTimeStamp;
+var oldURL;
+var calculation;
 
-// Scenario
-// user is on google.com
-// about to switch to bing.com which they have listed
-// after hitting return and page loads, then we have time from onCompleted
-// switch back to google.com, then we have time from onBeforeNavigate 
+// A way to check if user actually changed the page:
+//      check if they committed something
+//      AND if a new page completely loaded onCompleted
+
+// Scenario: blacklisted site --> notblacklisted site
+// on YouTube
+//      isBlacklisted = true
+// go to Reddit
+//      would go into for loop
+//      would go into else statement
+//      would change isBlacklisted to false
+//      if isBlacklisted is true, then take next URL and compare it to old URL
+//          if same, then do nothing
+//          if different, then take timeStamp
+//          and do calculation
+
+// Listens for whenever a page is fully loaded.
+chrome.webNavigation.onCompleted.addListener(
+    // take the timestamp of when a relevant page is fully loaded
+    (details) => {
+        console.log("onCompleted URL: " + details.url);
+
+        // Compares current URL to any of the blacklistedURLs
+        for (let i = 0; i < blacklistedURLs.length; i++)
+        {
+            if (details.url == blacklistedURLs[i]) {
+                console.log("This URL is blacklisted!");
+
+                isBlackListed = true;
+                oldURL = details.url;
+                startTimeStamp = details.timeStamp;
+
+                console.log("startTimeStamp: " + startTimeStamp);
+            } else {
+                console.log("This URL is not blacklisted!");
+
+                if (isBlackListed) {
+                    if (oldURL != details.url) {
+                        console.log("endTimeStamp: " + details.timeStamp);
+                        calculated = details.timeStamp - startTimeStamp;
+
+                        console.log("calculated: " + calculated);
+                    }
+                }
+            }
+        }
+    }
+);
 
 
-// Have conditional statement to check if user is on a relevant website 
-// all relevant websites stored in an array
 
-// calculate time stamp by onCompleted timestamp - onBeforeNavigate timestamp
+// // Listens for whenever a page is fully loaded.
+// chrome.webNavigation.onCompleted.addListener(
+//     // take the timestamp of when a relevant page is fully loaded
+//     (details) => {
+//         console.log("onCompleted URL: " + details.url)
+//         // console.log("onCompleted time: " + details.timeStamp);
+
+//         for (let i = 0; i < blacklistedURLs.length; i++) {
+
+//             console.log("in for loop, on " + i + "th iteration");
+//             console.log("blacklistedURL[" + i + "]:" + blacklistedURLs[i]);
+//             if (details.url == blacklistedURLs[i]) {
+//                 console.log("in the if statement");
+//                 console.log("onCompleted timeStamp: " + details.timeStamp);
+//                 onBlackListedURL = details.url;
+//                 onBlackListedStart = details.timeStamp;
+//             } else {
+//                 console.log("in the else statement");
+//                 console.log("onCompleted not timeStamp: " + details.timeStamp);
+//             }
+//         }
+
+
+//     }
+// );
 
 /* Gets all tabs that have the specified properties, or all tabs if no properties are specified. */
-chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-    var tab = tabs[0]; // a list of the different active tabs
-    var url = tab.url;
-    var domain = url.split('/')[2];
+// chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+//     var tab = tabs[0]; // a list of the different active tabs
+//     var url = tab.url;
+//     var domain = url.split('/')[2];
 
-    // note: Might say that this code only works on one window
-    // for (let i = 0; i < tabs.length; i++) {
-    //     console.log("All tabs: " + tabs[i].url);
-    // };
+//     console.log("Current page: " + url);
 
-    console.log("Current page: " + url);
-    // console.log("Current domain: " + domain);
+//     // *** If-statement here to check if the url is a blacklisted url"
+//     for (let i = 0; i < blacklistedURLs.length; i++)
+//     {
+//         if (url == blacklistedURLs[i])
+//         {
+//             console.log("url is blacklisted");
+//         }
+//     }
 
-    // userActivityLog.push(url);
+//     // console.log("Current domain: " + domain);
 
-    // for (let i = 0; i < userActivityLog.length; i++) {
-    //     console.log(userActivityLog[i]);
-    // };
+//     // userActivityLog.push(url);
 
-    chrome.webNavigation.onCompleted.addListener(function() {
-        // take the timestamp of when a relevant page is fully loaded
-        console.log("onComplete, loaded new page");
-    });
+//     // for (let i = 0; i < userActivityLog.length; i++) {
+//     //     console.log(userActivityLog[i]);
+//     // };
 
-    chrome.webNavigation.onBeforeNavigate.addListener(function() {
-        // take the timestamp of when the tab is switches
-        console.log("onBeforeNavigate, about to switch to new page")
-    });
-})
+//     // If relevant URL, then take time of when it's completed.
+
+//     // chrome.webNavigation.onBeforeNavigate.addListener(
+//     //     (details) => { 
+//     //         // take the timestamp of when the tab is switches
+//     //         console.log("onBeforeNavigate URL: " + details.url)
+//     //         console.log("onBeforeNavigate URL: " + details.timeStamp);
+//     //     }
+//     // );
+// })
